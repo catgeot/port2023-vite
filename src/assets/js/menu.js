a2a365cf75d4df884c9b21ab7ef88e4690ee6a3a
catgeot <catgeot@naver.com>
2023-11-03 16:39:48 +0900

    소개영역완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -2,12 +2,14 @@ export function menu(){
     const headerToggle = document.getElementById("headerToggle");
     const headerNav = document.querySelector(".header_nav");
 
-    headerToggle.addEventListener("click", () => {
-        headerNav.classList.toggle("show");
-
-        headerToggle.getAttribute("aria-expanded") ==="true" 
-        ? headerToggle.setAttribute("aria-expanded", "false") 
-        : headerToggle.setAttribute("aria-expanded", "true"); 
-        
-    });
+    if(headerToggle){
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+    
+            headerToggle.getAttribute("aria-expanded") ==="true" 
+            ? headerToggle.setAttribute("aria-expanded", "false") 
+            : headerToggle.setAttribute("aria-expanded", "true");         
+        });
+    }
+    
 }
