3ad7dad53d24564d8367880bc260a57bdd565748
catgeot <catgeot@naver.com>
2023-11-08 08:04:41 +0900

    포트폴리오 완성

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,7 +1,23 @@
-export function port(){
-    console.log(port)
-}
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-// export function port() {
-//     console.log(port)
-// }
+export function port() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horSection = gsap.utils.toArray(".port_item");
+
+    gsap.to(horSection, {
+        xPercent: -120 * (horSection.length -1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start: "top 90px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: false,  
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
+    });
+}
