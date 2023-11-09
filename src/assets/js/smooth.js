34f1a932fcc4b81724fb2cb2852a7693c0a6fc8c
catgeot <catgeot@naver.com>
2023-11-10 07:38:39 +0900

    마무리완료

--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,7 +1,22 @@
-export function smooth(){
-    console.log(smooth)
-}
+import Lenis from '@studio-freight/lenis';
 
-// export function smooth() {
-//     console.log(smooth)
-// }
+export function smooth(){
+    const lenis = new Lenis({
+        duration: 1,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+
+    
+
+    function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on('scroll', (e) => {
+        console.log(e);
+    })
+
+}
